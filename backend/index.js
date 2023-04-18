20c130a0454815a8092f1479edad4f746422bb36
Will <willjohnsmithiii@gmail.com>
2023-04-18 15:37:09 -0600

    prepare app for herokup deployment

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,59 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const axios = require("axios");
+const Movie = require("./models/movie");
+require("dotenv").config();
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const Movie = require("./models/movie");
-
-
 app.get("/api/movies", async (req, res) => {
-  const movies = await Movie.find();
-  res.json(movies);
+  const { search } = req.query;
+  const apiKey = process.env.TMDB_API_KEY;
+  const url = search
+    ? `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${search}`
+    : `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    const movies = response.data.results.map((movie) => ({
+      id: movie.id,
+      title: movie.title,
+      releaseYear: new Date(movie.release_date).getFullYear(),
+    }));
+    res.json(movies);
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    res.status(500).json({ message: "Error fetching movies" });
+  }
 });
+
+app.post("/api/favorites", async (req, res) => {
+  const { tmdbId, title, releaseYear } = req.body;
+  try {
+    const newFavorite = new Movie({ tmdbId, title, releaseYear });
+    const savedFavorite = await newFavorite.save();
+    res.status(201).json(savedFavorite);
+  } catch (error) {
+    console.error("Error adding favorite movie:", error);
+    res.status(500).json({ message: "Error adding favorite movie" });
+  }
+});
+
+app.get("/api/favorites", async (req, res) => {
+  try {
+    const favoriteMovies = await Movie.find();
+    res.json(favoriteMovies);
+  } catch (error) {
+    console.error("Error fetching favorite movies:", error);
+    res.status(500).json({ message: "Error fetching favorite movies" });
+  }
+});
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
--- backend/models/movie.js
+++ backend/models/movie.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const movieSchema = new mongoose.Schema({
+  tmdbId: Number,
   title: String,
   releaseYear: Number,
 });
